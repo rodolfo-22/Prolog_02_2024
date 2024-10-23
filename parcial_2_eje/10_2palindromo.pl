f176a1a884418005aae8be587b9bdc466021a2de
rodolfo <00082421@uca.edu.sv>
2024-10-23 01:30:55 -0600

    modify

--- parcial_2_eje/10_2palindromo.pl
+++ parcial_2_eje/10_2palindromo.pl
@@ -1,5 +1,6 @@
 %palindromo
 % reverse, es un predicado predefinido en Prolog que invierte una lista.
+% en py podemos verificar solo la mitad de la cadena para determinar si es palindromo
 es_palindromo(L, true) :- 
      reverse(L, L). % Si la lista es igual a su reversa, es un palíndromo.
 es_palindromo(_, false). % Si no es igual, no es un palíndromo.
